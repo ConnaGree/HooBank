8b9b98568ae5f749ba5a6160d8aeac95c587c102
Zerubabel <zerubabelyon@gmail.com>
2024-05-27 03:15:14 +0530

    hoobank v1

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import styles from "../style";
+import { discount, robot } from "../assets";
+
+const Hero = () => (
+  <section
+    id="home"
+    className={`flex md:flex-row justify-between pl-20 flex-col ${styles.paddingY}`}
+  ></section>
+);
+export default Hero;
